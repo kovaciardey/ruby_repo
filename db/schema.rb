591fed8823c94978bcbb47de400d85d30e69c50c
Andrei Kovaci <andrei.kovaci@gmail.com>
2021-10-31 22:45:58 +0000

    added one to many relationship to users and articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_31_221652) do
+ActiveRecord::Schema.define(version: 2021_10_31_222656) do
 
   create_table "articles", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "user_id"
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
