c88bec68f0e2850249f70bd7817e30c1365c5ecb
Andrei Kovaci <andrei.kovaci@gmail.com>
2021-10-31 20:29:22 +0000

    added new fields to the articles table

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_31_200142) do
+ActiveRecord::Schema.define(version: 2021_10_31_202352) do
 
   create_table "articles", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
